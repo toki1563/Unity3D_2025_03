8cdcc4b0c4d31bfb78e16b7cb9d8d78ed1000b72
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-03-15 02:35:48 +0900

    3/15_テスト

--- Assets/_Iwaki/Scripts/System/E_GameStateReceiverTest.cs
+++ Assets/_Iwaki/Scripts/System/E_GameStateReceiverTest.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 /// <summary>
 /// ゲームオーバー・ステージクリア通知のテスト
 /// </summary>
-public class E_TestObj : MonoBehaviour, IGameOverObserver, IStageClearObserver
+public class E_GameStateReceiverTest : MonoBehaviour, IGameOverObserver, IStageClearObserver, IGameStartObserver
 {
     public async void OnGameOver()
     {
         await Task.Delay(1000);
         Debug.Log($"{name} GameOver");
+    }
+
+    public async void OnGameStart()
+    {
+        await Task.Delay(1000);
+        Debug.Log($"{name} GameStart");
     }
 
     public async void OnStageClear()
