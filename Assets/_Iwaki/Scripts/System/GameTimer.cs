ce49a5258d87a763db2522e2e233b1c10df3740e
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-03-14 20:51:53 +0900

    3/14_GameTimer

---
+++ Assets/_Iwaki/Scripts/System/GameTimer.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// ステージ内の制限時間の管理、タイムアップの通知を行うクラス。
+/// </summary>
+public class GameTimer : MonoBehaviour, IGameOverSender
+{
+    [SerializeField] bool enableTimer;
+    [SerializeField, Header("制限時間(秒)")] float timeLimit = 180;
+    [SerializeField] Text timerText;
+
+    float currentTimer;
+
+    public event Action SendGameOver;
+
+    private void Start()
+    {
+        currentTimer = timeLimit;
+        UpdateTimerText();
+    }
+
+    private void Update()
+    {
+        if (enableTimer)
+        {
+            currentTimer -= Time.deltaTime;
+
+            if (currentTimer <= 0)
+            {
+                currentTimer = 0;
+                TimerStop();
+                SendGameOver.Invoke();
+            }
+
+            UpdateTimerText();
+        }
+    }
+
+    private void UpdateTimerText()
+    {
+        var timeSpan = TimeSpan.FromSeconds(currentTimer);
+        var formattedTime = timeSpan.ToString(@"mm\:ss\.ff");
+        timerText.text = formattedTime;
+    }
+
+    public void TimerStart()
+    {
+        enableTimer = true;
+    }
+
+    public void TimerStop()
+    {
+        enableTimer = false;
+    }
+}
