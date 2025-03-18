aec421f1a8bc0b45431c92183c0c672f3627c6d5
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-03-19 01:53:47 +0900

    3/18_HPBarUI実装

--- Assets/_Iwaki/Scripts/UI/HPBarUI.cs
+++ Assets/_Iwaki/Scripts/UI/HPBarUI.cs
@@ -1,23 +1,48 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// プレイヤー体力UI
 /// </summary>
 public class HPBarUI : MonoBehaviour
 {
-    [SerializeField] R_PlayerManager playerManager;
+    [SerializeField] Slider health;
+    [SerializeField] Slider damage;
+    [SerializeField] float timeToStartDecreaseDamageBar = 1;
+    [SerializeField] float damageBarDecreaseDuration = 1;
+ 
+    R_PlayerManager playerManager;
+    float prevHP;
+    float prevDamagedTime;
+    float targetRatio = 1;
 
-    private void Reset()
+    private void Start()
     {
-        if (FindAnyObjectByType<R_PlayerManager>() is R_PlayerManager playerManager)
-        {
-            this.playerManager = playerManager;
-        }
+        playerManager = R_PlayerManager.Instance;
+        prevHP = playerManager._MaxHP;
+
+        health.value = 1;
+        damage.value = 1;
     }
 
     private void Update()
     {
-        var ratio = playerManager._CurrentHP / playerManager._MaxHP;
+        if (playerManager._CurrentHP != prevHP)
+        {
+            prevDamagedTime = Time.time;
+            prevHP = playerManager._CurrentHP;
+            UpdateView();
+        }
 
+        if (prevDamagedTime + timeToStartDecreaseDamageBar < Time.time)
+        {
+            damage.value = Mathf.MoveTowards(damage.value, targetRatio, damageBarDecreaseDuration * Time.deltaTime);
+        }
+    }
+
+    private void UpdateView()
+    {
+        targetRatio = playerManager._CurrentHP / playerManager._MaxHP;
+        health.value = targetRatio;
     }
 }
